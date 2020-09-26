9462bbc78d0a86a84f2ea1bf080b952ed25437b6
Federico Castro García <fede.castrog@gmail.com>
2020-09-26 02:37:21 -0300

    Restructure folder & configure webpack reloader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  mode: 'production',
+const CopyPlugin = require('copy-webpack-plugin')
+const ExtensionReloader = require('webpack-extension-reloader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const config = {
+  mode: process.env.NODE_ENV,
+  context: __dirname + '/',
+  devtool: 'cheap-module-source-map',
   entry: {
     background: './src/scripts/background.js',
     popup: './src/popup/index.js',
@@ -21,17 +27,34 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/i,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
-          'resolve-url-loader',
           'sass-loader',
+          'resolve-url-loader',
         ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.scss'],
-  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/popup/index.html', to: 'popup.html' },
+        {
+          from: '**/*',
+          context: 'public',
+        },
+      ],
+    }),
+  ],
 }
+
+if (process.env.NOVE_ENV === 'development') {
+  config.plugins = (config.plugins || []).concat([new ExtensionReloader()])
+}
+
+module.exports = config
